4aa60a03a5756c910841ef210268b6403ec12eba
Nguyen Thanh Do <do.nt@gianty.com.vn>
2025-01-06 16:59:54 +0700

    Commit

--- docs/_next/static/chunks/app/policy/page-b6bbe4654a0bfdca.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[66,781,974],{1302:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,7970,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[970,441,517,358],()=>s(1302)),_N_E=e.O()}]);
